2700c242faff5b08ef8443f8d64e33c3efd1dfc2
Sula M <SuleimanMusa@yandex.com>
2025-06-09 12:30:31 +0200

    final-project sprint 6

--- src/tasksapp/InMemoryHistoryManager.java
+++ src/tasksapp/InMemoryHistoryManager.java
@@ -1,25 +1,90 @@
 package tasksapp;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager{
 
-    private final List<Task> history = new ArrayList<>();
-    private static final int MAX_HISTORY_SIZE = 10;
+    private Node<Task> head;
+    private Node<Task> tail;
+
+    private final Map<Integer, Node<Task>> nodeMap = new HashMap<>();
 
     @Override
     public void add(Task task) {
-        if (task != null) {
-            history.add(task);
-            if (history.size() > MAX_HISTORY_SIZE) {
-                history.removeFirst();
-            }
+        if (task == null) return;
+
+        remove(task.getId()); //удаление предыдущего просмотра
+
+        Node<Task> newNode = new Node<>(task); //создание нового узла
+        linkLast(newNode);
+
+        nodeMap.put(task.getId(), newNode);
+    }
+
+    @Override
+    public void remove(int id) {
+        Node<Task> node = nodeMap.remove(id);
+        if (node != null) {
+            removeNode(node);
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(history);
+        return getTasks();
+    }
+
+    private List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>();
+        Node<Task> current = head;
+        while (current != null) {
+            tasks.add(current.data);
+            current = current.next;
+        }
+        return tasks;
+    }
+
+
+
+    private void linkLast(Node<Task> node) {
+        if (tail == null) {
+            head = tail = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+    }
+
+    private void removeNode(Node<Task> node) {
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
+    }
+
+    private static class Node<T> {
+        T data;
+        Node<T> prev;
+        Node<T> next;
+
+        Node(Node<T> prev, T data, Node<T> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+
+        public Node(T data) {
+            this.data = data;
+        }
     }
 }
